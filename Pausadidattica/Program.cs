31eb1e98ce9f40aeeb32482d5d3fdf0b4e27fa3a
Simone Bassanelli <bassanelli.simone.studente@itispaleocapa.it>
2023-01-19 22:21:40 +0100

    Finte funzioni tranne ultime due

--- Pausadidattica/Program.cs
+++ Pausadidattica/Program.cs
@@ -11,16 +11,17 @@ namespace Pausadidattica
         static void Main(string[] args)
         {
             int[] arr = new int[100];
-            int b, nel, e;
+            int nel, h;
             int dim = 0;
             nel = 0;
             int scelta = 0;
+            h = 0;
 
             //Struttura menù
             do
             {
                 //stampa delle opzioni
-                Console.Clear(); 
+                Console.Clear();
                 Console.WriteLine("1 - Aggiungi elemento");
                 Console.WriteLine("2 - Stampa elementi caricati");
                 Console.WriteLine("3 - Visualizzazione array in HTML");
@@ -36,7 +37,7 @@ namespace Pausadidattica
                         Console.WriteLine($"Inserisci l'elemento ");
                         int ele = int.Parse(Console.ReadLine());
                         AddArray(arr, ele, ref dim);
-                        if( AddArray(arr, ele, ref dim) == true )
+                        if (AddArray(arr, ele, ref dim) == true)
                         {
                             Console.WriteLine("Eleneto inseriro correttamente");
                         }
@@ -53,17 +54,44 @@ namespace Pausadidattica
                         }
                         Console.ReadLine();
                         break;
+
+                    case 3:
+                        Console.WriteLine("Elemento da cercare: ");
+                        int l = int.Parse(Console.ReadLine());
+                        if (Funzionericerca(arr, l, h) == true)
+                        {
+                            Console.WriteLine("Elemento presente, posizione numero: " + h);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Elemento non presente nell'array, -1 ");
+                        }
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Elemento da eliminare: ");
+                        int c = int.Parse(Console.ReadLine());
+                        if (Eleminazionelemento(arr, c) == true)
+                        {
+                            Console.WriteLine("Valore eliminato");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Errore valore non trovato in quanto non presente");
+                        }
+                        break;
+
                 }
                 Console.ReadLine();
             }
             while (scelta != 0);
-            
+
             Console.WriteLine("Elementi da caricare");
             nel = int.Parse(Console.ReadLine());
         }
 
         //Aggiungere in coda un elemento all'array (interi)
-        static bool AddArray(int[] a, int e,ref int index)
+        static bool AddArray(int[] a, int e, ref int index)
         {
             //Controllo se abbiamo raggiunto la dimensione massima
             bool inserito = true;
@@ -83,23 +111,50 @@ namespace Pausadidattica
         //Visualizzazione dell'array che restituisca la stringa in HTML
         public static void Funzione2(string[] args)
         {
-            
+
         }
 
         //Ricerca un numero all'interno dell'array, la funzione deve restituire la posizione dell'elemento se lo trova, -1 se non lo trova;
-        public static int FunzioneRicerca(int[] a, int chiave)
+        static bool Funzionericerca(int[] a, int l, int h)
         {
-            int occorrenze = 0;
-            for (int i = 0; i < 100; i++)
-                if (a[i] == chiave)
-                    occorrenze++;
-            return occorrenze;
+            bool selezionato = true;
+            //ricerca nell'array 
+            for (int i = 0; i < a.Length; i++)
+            {
+
+                //Confronto singolo elemento 
+                if (l == a[i])
+                {
+                    h = i;
+                    return selezionato;
+                }
+                else
+                {
+                    selezionato = false;
+                }
+            }
+            return selezionato;
         }
 
         //Cancellazione di un elemento dell'array
-        public static void Funzione4(string[] args)
+        static bool Eleminazionelemento(int[] a, int c)
         {
-
+            bool selezionato = true;
+            //ricerca nell'array 
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (c == a[i])
+                {
+                    //Se l'elemento è contenuto nell'array lo vado ad eliminare
+                    a[i] = a[i] - c;
+                    return selezionato;
+                }
+                else
+                {
+                    selezionato = false;
+                }
+                return selezionato;
+            }
         }
 
         //Inserimento di un valore in una posizione dell'array
